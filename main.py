cdd3c91e93060e1c8dfcce917787fe181b82fdb7
Kulingmas <81082549+Kulingmas@users.noreply.github.com>
2022-01-03 12:08:37 +0700

    main

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ client = commands.Bot(
 
 @client.event
 async def on_connect():
-  await client.change_presence(activity = discord.Streaming(name = "24/7 Online ┊ 6AM - 10PM Online ┊ Late Respond ┊ Puasa bor.", url = "https://www.twitch.tv/become_capsl"))
+  await client.change_presence(activity = discord.Streaming(name = "24/7 Online ┊ 6AM - 10PM Online ┊ fast Respond ┊ Happy New Year 2022.", url = "https://www.twitch.tv/become_capsl"))
 
 
 keep_alive.keep_alive()
